db1fdd039b912a25bf63eb13fa60553ec7f0fd9b
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-04-28 00:11:21 +0200

    oki

---
+++ ok/background.js
@@ -0,0 +1,8 @@
+// background.js
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+    chrome.tabs.executeScript({
+      code: 'document.body.style.backgroundColor="red";'
+    });
+  });
+  
