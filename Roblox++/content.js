889fae4cc14b61d39f3216736b64a79d4438b12a
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-04-30 01:21:51 +0200

    oki

--- Roblox++/content.js
+++ Roblox++/content.js
@@ -1,5 +1,6 @@
 const button = document.createElement("button");
 const link = document.createElement("link");
+const colorPicker = document.createElement("input");
 
 link.rel = "stylesheet";
 link.href = chrome.extension.getURL("style.css");
@@ -7,6 +8,10 @@ document.head.appendChild(link);
 
 button.innerHTML = "Roblox++";
 button.classList.add("rohydr-button");
+
+colorPicker.type = "color";
+colorPicker.value = "#FFFFFF";
+colorPicker.classList.add("rohydr-colorpicker");
 
 let popup = null;
 let isPopupVisible = false;
@@ -25,15 +30,10 @@ button.addEventListener("click", () => {
   popup = document.createElement("div");
   popup.classList.add("rohydr-popup");
   popup.innerHTML = `
-    <div class="rohydr-popup-header">
-      <h3>Roblox++ Popup</h3>
-      <label class="rohydr-toggle">
-        <input type="checkbox">
-        <span class="rohydr-toggle-slider"></span>
-      </label>
-    </div>
     <div class="rohydr-popup-content">
-      <p>This is the popup content.</p>
+      <label for="rohydr-colorpicker">Background Color</label>
+      <br>
+      <input type="color" id="rohydr-colorpicker" value="#FFFFFF">
     </div>
   `;
 
@@ -41,20 +41,50 @@ button.addEventListener("click", () => {
   const buttonRect = button.getBoundingClientRect();
   popup.style.top = `${buttonRect.bottom}px`;
   popup.style.left = `${buttonRect.left}px`;
+
+  // Add event listener to color picker
+  const colorPickerElement = popup.querySelector("#rohydr-colorpicker");
+  colorPickerElement.addEventListener("change", () => {
+    const mainContainer = document.querySelector("#container-main");
+    const contentContainer = document.querySelector("#container-main > div.content");
+    const profileContainer = document.querySelector("#container-main > div.content > div.profile-container.ng-scope");
+    const profileHeader = document.querySelector("#container-main > div.content > div.profile-container.ng-scope > div > div.section.profile-header > div");
+
+    mainContainer.style.backgroundColor = colorPickerElement.value;
+    contentContainer.style.backgroundColor = colorPickerElement.value;
+
+    if (profileContainer) {
+      profileContainer.style.backgroundColor = colorPickerElement.value;
+    }
+
+    if (profileHeader) {
+      profileHeader.style.backgroundColor = colorPickerElement.value;
+    }
+
+    localStorage.setItem("rohydr-color", colorPickerElement.value);
+  });
+
   setTimeout(() => {
     popup.classList.add("show");
     isPopupVisible = true;
   }, 0);
 });
 
-window.addEventListener("scroll", () => {
-  if (isPopupVisible) {
-    const buttonRect = button.getBoundingClientRect();
-    popup.style.top = `${buttonRect.bottom}px`;
-    popup.style.left = `${buttonRect.left}px`;
-    popup.style.position = "fixed";
-  }
-});
-
 const navbar = document.querySelector(".navbar-right");
 navbar.prepend(button);
+
+const storedColor = localStorage.getItem("rohydr-color");
+if (storedColor) {
+  colorPicker.value = storedColor;
+  const mainContainer = document.querySelector("#container-main");
+  mainContainer.style.backgroundColor = storedColor;
+  const profileContainer = document.querySelector("#container-main > div.content > div.profile-container.ng-scope");
+  profileContainer.style.backgroundColor = storedColor;
+  const profileHeader = document.querySelector("#container-main > div.content > div.profile-container.ng-scope > div > div.section.profile-header > div");
+  profileHeader.style.backgroundColor = storedColor;
+  const contentContainer = document.querySelector("#container-main > div.content");
+  console.log(contentContainer); // Debugging line
+  contentContainer.style.backgroundColor = storedColor;
+  console.log(contentContainer.style.backgroundColor); // Debugging line
+  
+}
